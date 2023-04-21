414420b26da87831a34a3655abbebb92bdb35292
k022g1052@g.neec.ac.jp <k022g1052@g.neec.ac.jp>
2023-04-21 10:29:22 +0900

    02_01_確認課題

---
+++ Player.cpp
@@ -0,0 +1,33 @@
+﻿#include "Player.h"
+
+/// <summary>
+/// 初期化
+/// </summary>
+/// <param name="model"></param>
+/// <param name="textureHandle"></param>
+void Player::Initialize(Model* model, uint32_t textureHandle) {
+	// NULLポイントチェック
+	assert(model);
+
+	// 受け渡し
+	model_ = model;
+	textureHandle_ = textureHandle;
+
+	// ワールド変換の初期化
+	worldTransform_.Initialize();
+}
+
+/// <summary>
+/// 更新
+/// </summary>
+void Player::Update() {
+	// 行列を定数バッファーに転送
+	worldTransform_.TransferMatrix();
+}
+/// <summary>
+/// 描画
+/// </summary>
+void Player::Draw(ViewProjection& viewProjection)
+ {
+	model_->Draw(worldTransform_, viewProjection, textureHandle_);
+ }
--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -4,16 +4,37 @@
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() { 
+	delete model_;
+	delete player_;
+}
 
 void GameScene::Initialize() {
 
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
+
+	//ファイル名を指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("sample.png");
+
+	// モデルの生成
+	model_ = Model::Create();
+
+	//ビュープロジェクションの初期化
+	viewProjection_.Initialize();
+
+	//自キャラの生成
+	player_ = new Player();
+	//自キャラの初期化
+	player_->Initialize(model_, textureHandle_);
 }
 
-void GameScene::Update() {}
+void GameScene::Update() 
+{
+	//自キャラの更新
+	player_->Update();
+}
 
 void GameScene::Draw() {
 
@@ -41,7 +62,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
-
+	player_->Draw(viewProjection_);
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
 #pragma endregion
@@ -58,4 +79,7 @@ void GameScene::Draw() {
 	Sprite::PostDraw();
 
 #pragma endregion
+
+
+
 }
